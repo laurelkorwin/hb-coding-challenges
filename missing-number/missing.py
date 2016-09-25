5c9e619eb752e47def7bc08b919cb17d22c6f4ab
Laurel <lkorwin@gmail.com>
2016-09-25 23:52:19 +0000

    Finished two more challenges

--- missing-number/missing.py
+++ missing-number/missing.py
@@ -8,7 +8,7 @@ def missing_number(nums, max_num):
     *max_num*: Largest potential number in list
     """
 
-    seen = [False] * max_num 
+    seen = [False] * max_num
     # [False, False, False]
 
     for n in nums:
